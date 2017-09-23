5a0f984b2eeba25675d81d4a3a987d8f08197c10
victor nitu <nitu.v.r@gmail.com>
2017-09-23 14:42:28 +0200

    Update fish git settings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,38 @@
+set normal (set_color normal)
+set magenta (set_color magenta)
+set yellow (set_color yellow)
+set green (set_color green)
+set red (set_color red)
+set gray (set_color -o black)
+
+# Fish git prompt
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showstashstate 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_color_branch yellow
+set __fish_git_prompt_color_upstream_ahead green
+set __fish_git_prompt_color_upstream_behind red
+
+# Status Chars
+set __fish_git_prompt_char_dirtystate '*'
+set __fish_git_prompt_char_stagedstate '→'
+set __fish_git_prompt_char_untrackedfiles '?'
+set __fish_git_prompt_char_stashstate '↩'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
+
 function fish_prompt
     set -l git_branch (git branch ^/dev/null | sed -n '/\* /s///p')
     set -g fish_prompt_pwd_dir_length 0
-    echo -n (set_color green)(whoami)(set_color white)'@'(set_color blue)(hostname | cut -d . -f 1)(set_color white)':'(set_color cyan)(prompt_pwd)(set_color white)'('(set_color purple)"$git_branch"(set_color white)') > '
+	printf '%s' (set_color green)(whoami)
+    printf '%s' (set_color white)'@'
+	printf '%s' (set_color blue)(hostname | cut -d . -f 1)
+	printf '%s' (set_color white)':'
+	printf '%s' (set_color cyan)(prompt_pwd)
+	printf '%s' (set_color white)':'
+	printf '%s' (__fish_git_prompt)
+	printf '%s' (set_color white)' > '
 end
 
 function fish_right_prompt -d "Write out the right prompt"
